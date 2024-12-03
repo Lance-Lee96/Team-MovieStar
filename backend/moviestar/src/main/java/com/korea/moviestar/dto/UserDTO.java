dba85977ed95726636b6f36d641643a765d67779
zlzkzlgh <zlzkzlgh@gmail.com>
2024-12-03 17:44:02 +0900

    backend
    backend

---
+++ backend/moviestar/src/main/java/com/korea/moviestar/dto/UserDTO.java
@@ -0,0 +1,43 @@
+package com.korea.moviestar.dto;
+
+import java.util.List;
+
+import com.korea.moviestar.entity.UserEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+	private int userId;
+	private String userName;
+	private String userNick;
+	private String userEmail;
+	private String userPwd;
+	private List<Integer> userLikeList;
+	
+	public UserDTO(UserEntity entity) {
+		this.userId = entity.getUserId();
+		this.userNick = entity.getUserNick();
+		this.userName = entity.getUserName();
+		this.userEmail = entity.getUserEmail();
+		this.userPwd = entity.getUserPwd();
+		this.userLikeList = entity.getUserLikeList();
+	}
+	
+	public static UserEntity toEntity(UserDTO dto) {
+		return UserEntity.builder()
+					.userId(dto.getUserId())
+					.userNick(dto.getUserNick())
+					.userName(dto.getUserName())
+					.userEmail(dto.getUserEmail())
+					.userPwd(dto.getUserPwd())
+					.userLikeList(dto.getUserLikeList())
+					.build();
+	}
+}
