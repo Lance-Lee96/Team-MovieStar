c4947785288fc636f200f9d428a7d50af5dac6f7
Jiheon <wlgjs566@gmail.com>
2024-12-04 12:48:00 +0900

    userLikeList 수정
    리스트에서 배열로 수정

--- backend/moviestar/src/main/java/com/korea/moviestar/dto/UserDTO.java
+++ backend/moviestar/src/main/java/com/korea/moviestar/dto/UserDTO.java
@@ -19,7 +19,7 @@ public class UserDTO {
 	private String userNick;
 	private String userEmail;
 	private String userPwd;
-	private List<Integer> userLikeList;
+	private int[] userLikeList;
 	private String token;
 	
 	public UserDTO(UserEntity entity) {
